bb484dc55cd6f1868b534498e2ec040909b7ea7e
gustafsonb <gustafsonb37@gmail.com>
2014-07-24 22:08:26 -0500

    revision 5
    added some notes

--- application.js
+++ application.js
@@ -1,3 +1,12 @@
 function showTextBoxes() {
-	alert("Thank you for clicking there!!!");
+	var box1 = $("#one").val();
+	var box2 = $("#two").val();
+	var box3 = $("#three").val();
+	
+	<!-- javascript structure -->
+	
+	alert(box1 + box2 + box3);
+	
+	$(".odd").val("");
+	$(".even").val("");
 }
